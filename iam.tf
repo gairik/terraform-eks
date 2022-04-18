94f26c162e93603a323da33ee56b75dd2a89b6c3
Gijon Alchemy <pitonrok@gmail.com>
2022-04-18 22:37:42 +0200

    Added tls and openid creation using terraform

--- iam.tf
+++ iam.tf
@@ -55,3 +55,12 @@ EOF
 
 }
 
+data "tls_certificate" "example" {
+  url = aws_eks_cluster.demo.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "example" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.example.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.demo.identity[0].oidc[0].issuer
+}
